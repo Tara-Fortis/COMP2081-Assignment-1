01b9edc50cc8d82219d35916fd6177399d515e4f
Tineil_A <tine6573@gmail.com>
2025-10-06 22:05:31 -0400

    Added a search bar

--- scripts.js
+++ scripts.js
@@ -1,6 +1,2 @@
-const myCarouselElement = document.querySelector('#myCarousel')
-
-const carousel = new bootstrap.Carousel(myCarouselElement, {
-  interval: 2000,
-  touch: false
-})
+// search the page for input
+searchTerms = mySearch.value;
